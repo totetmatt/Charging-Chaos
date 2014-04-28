965e149603b59ac9bccf2c636e9f2c470b5b9e76
Matthieu Totet <matthieu.totet@gmail.com>
2014-04-28 23:01:08 +0200

    No infinite loop

--- main.pl
+++ main.pl
@@ -7,23 +7,22 @@ configOk([Ha|Ta],[Hb|Tb]):-
 solve(OUTLET,DEVICE,SWITCH):-
     false.
 
-switching([A],S,O):-
-    write(A),
-    write(S),
+switching([A],S,[O]):-
+   
     rev(A,S,O).
 switching([HOUTLET|TOUTLET],SWITCH,[HO|TO]):-
+    write(HOUTLET),
     switching(TOUTLET,SWITCH,TO),
-    rev(HOUSTLET,SWITCH,HO),!.
+    rev(HOUSTLET,SWITCH,HO).
     
    
-    
+rev([H|T],1,[Hb|T]):-
+    rev(H,Hb).   
 rev([H|T],SWITCH,[H|X]):-
     S is SWITCH - 1,
     rev(T,S,X).
 
-rev([H|T],1,[Hb|T]):-
-    rev(H,Hb),
-    !.
+
 
 
 rev(1,0).
